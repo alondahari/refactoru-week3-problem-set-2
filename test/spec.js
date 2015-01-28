140a2ffd1494d6cc7ced53589478ed6ebb496e33
Alon Dahari <climbinghobo@gmail.com>
2015-01-28 16:52:59 -0700

    wrote tests

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,19 @@
-describe('module', function() {
-  it('should do something', function() {
-    expect(true).toEqual(false);
+describe('problem set 2', function() {
+  describe("firstReverse", function () {
+    it('should take a string and reverse it', function() {
+      expect(firstReverse("Hi There!").toBe("!erehT iH"));
+    });
+  });
+
+  describe("swapCase", function () {
+    it("should toggle the cases of a string", function () {
+      expect(swapCase("Hello World").toBe("hELLO wORLD"));
+    });
+  });
+
+  describe("letterCount", function () {
+    it("should return the first word with the greatest number of repeated letters", function () {
+      expect(letterCount('Today, is the greatest day ever!' ).toBe('greatest'));
+    });
   });
 });
